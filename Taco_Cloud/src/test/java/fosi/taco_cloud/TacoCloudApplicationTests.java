ef164dd71688f2e3e2d6da9cab4cd8ce9cda0c58
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-09-15 16:51:28 +0300

    Initial commit

---
+++ Taco_Cloud/src/test/java/fosi/taco_cloud/TacoCloudApplicationTests.java
@@ -0,0 +1,15 @@
+package fosi.taco_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
