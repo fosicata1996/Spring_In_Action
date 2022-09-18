dca4ba003bd7ad8604194113909a331ba33de15b
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-09-18 17:34:09 +0300

    Chapter 2 - Validating form input

--- Taco_Cloud/src/main/java/fosi/taco_cloud/controller/tacos/web/OrderController.java
+++ Taco_Cloud/src/main/java/fosi/taco_cloud/controller/tacos/web/OrderController.java
@@ -3,11 +3,14 @@ package fosi.taco_cloud.controller.tacos.web;
 import fosi.taco_cloud.entity.tacos.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -22,8 +25,13 @@ public class OrderController
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus)
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus)
     {
+        if (errors.hasErrors())
+        {
+            return "orderForm";
+        }
+
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
 
