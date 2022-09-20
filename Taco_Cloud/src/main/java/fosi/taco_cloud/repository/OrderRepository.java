4961c4b806f8c50620c3b8903299a9665a1eb689
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-09-20 15:58:30 +0300

    Chapter 3 - Spring Data JPA - Custom queries

--- Taco_Cloud/src/main/java/fosi/taco_cloud/repository/OrderRepository.java
+++ Taco_Cloud/src/main/java/fosi/taco_cloud/repository/OrderRepository.java
@@ -1,8 +1,22 @@
 package fosi.taco_cloud.repository;
 
 import fosi.taco_cloud.entity.TacoOrder;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
+import java.util.List;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long>
 {
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
+    List<TacoOrder> findByDeliveryNameAndDeliveryCityAllIgnoreCase(String deliveryName, String deliveryCity);
+
+    List<TacoOrder> findByDeliveryCityOrderByDeliveryName(String city);
+
+    @Query("SELECT o from TacoOrder o where o.deliveryCity='Seattle'")
+    List<TacoOrder> readOrdersDeliveredInSeattle();
 }
--- Taco_Cloud/src/main/resources/import.sql
+++ Taco_Cloud/src/main/resources/import.sql
@@ -1,20 +1,10 @@
-insert into Ingredient
-VALUES ('FLTO', 'Flour Tortilla', 'WRAP');
-insert into Ingredient
-VALUES ('COTO', 'Corn Tortilla', 'WRAP');
-insert into Ingredient
-VALUES ('GRBF', 'Ground Beef', 'PROTEIN');
-insert into Ingredient
-VALUES ('CARN', 'Carnitas', 'PROTEIN');
-insert into Ingredient
-VALUES ('TMTO', 'Diced Tomatoes', 'VEGGIES');
-insert into Ingredient
-VALUES ('LETC', 'Lettuce', 'VEGGIES');
-insert into Ingredient
-VALUES ('CHED', 'Cheddar', 'CHEESE');
-insert into Ingredient
-VALUES ('JACK', 'Monterrey Jack', 'CHEESE');
-insert into Ingredient
-VALUES ('SLSA', 'Salsa', 'SAUCE');
-insert into Ingredient
-VALUES ('SRCR', 'Sour Cream', 'SAUCE');
+insert into Ingredient VALUES ('FLTO', 'Flour Tortilla', 'WRAP');
+insert into Ingredient VALUES ('COTO', 'Corn Tortilla', 'WRAP');
+insert into Ingredient VALUES ('GRBF', 'Ground Beef', 'PROTEIN');
+insert into Ingredient VALUES ('CARN', 'Carnitas', 'PROTEIN');
+insert into Ingredient VALUES ('TMTO', 'Diced Tomatoes', 'VEGGIES');
+insert into Ingredient VALUES ('LETC', 'Lettuce', 'VEGGIES');
+insert into Ingredient VALUES ('CHED', 'Cheddar', 'CHEESE');
+insert into Ingredient VALUES ('JACK', 'Monterrey Jack', 'CHEESE');
+insert into Ingredient VALUES ('SLSA', 'Salsa', 'SAUCE');
+insert into Ingredient VALUES ('SRCR', 'Sour Cream', 'SAUCE');
