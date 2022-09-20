8b29c548abbf0dce8461d7dcdc77c9cf7698f28e
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-09-20 18:55:02 +0300

    Chapter 4 - Spring Data MongoDB

--- Taco_Cloud/src/main/java/fosi/taco_cloud/entity/Ingredient.java
+++ Taco_Cloud/src/main/java/fosi/taco_cloud/entity/Ingredient.java
@@ -4,26 +4,20 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Id;
-import java.io.Serializable;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Entity
+@Document(collection = "ingredients")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
-public class Ingredient implements Serializable
+public class Ingredient
 {
-    private static final long serialVersionUID = 1L;
 
     @Id
     private String id;
     private String name;
 
-    @Enumerated(EnumType.STRING)
     private Type type;
 
     public enum Type
--- Taco_Cloud/src/main/java/fosi/taco_cloud/repository/OrderRepository.java
+++ Taco_Cloud/src/main/java/fosi/taco_cloud/repository/OrderRepository.java
@@ -1,22 +1,8 @@
 package fosi.taco_cloud.repository;
 
 import fosi.taco_cloud.entity.TacoOrder;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Date;
-import java.util.List;
-
-public interface OrderRepository extends CrudRepository<TacoOrder, Long>
+public interface OrderRepository extends CrudRepository<TacoOrder, String>
 {
-    List<TacoOrder> findByDeliveryZip(String deliveryZip);
-
-    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
-
-    List<TacoOrder> findByDeliveryNameAndDeliveryCityAllIgnoreCase(String deliveryName, String deliveryCity);
-
-    List<TacoOrder> findByDeliveryCityOrderByDeliveryName(String city);
-
-    @Query("SELECT o from TacoOrder o where o.deliveryCity='Seattle'")
-    List<TacoOrder> readOrdersDeliveredInSeattle();
 }
