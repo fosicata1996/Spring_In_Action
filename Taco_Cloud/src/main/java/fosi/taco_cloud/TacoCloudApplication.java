22a98517703f2402adeb2c76e13014f4d0227c84
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-09-22 15:44:34 +0300

    Chapter 5 - Spring Security - Conditionally creating beans with profiles

--- Taco_Cloud/src/main/java/fosi/taco_cloud/TacoCloudApplication.java
+++ Taco_Cloud/src/main/java/fosi/taco_cloud/TacoCloudApplication.java
@@ -1,15 +1,32 @@
 package fosi.taco_cloud;
 
+import fosi.taco_cloud.repository.IngredientRepository;
+import fosi.taco_cloud.repository.security.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@Profile("!qa")
 public class TacoCloudApplication
 {
 
     public static void main(String[] args)
     {
         SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+    @Bean
+    //    @Profile("dev")
+    //    @Profile({"dev", "qa"})
+    @Profile("!prod")
+    public CommandLineRunner dataLoader(IngredientRepository ingredientRepository, UserRepository userRepository,
+        PasswordEncoder encoder)
+    {
+        return null;
     }
 
 }
